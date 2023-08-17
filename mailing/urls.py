b34e0b2a6b08114ac7193eba78a52ccfe0c5b207
Dante <dante1201@mail.ru>
2023-08-18 00:11:32 +0300

    рограмма кешируется, добавлено приложени BLOG, реализована группа пользователей "Менеджер"

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from mailing.apps import MailingConfig
 from mailing.views import main, MailingCreateView, MailingListView, MailingDetailView, MailingUpdateView, \
@@ -9,7 +10,7 @@ app_name = MailingConfig.name
 urlpatterns = [
     path('', main, name='main'),
     path('create/', MailingCreateView.as_view(), name='create'),
-    path('mailing/', MailingListView.as_view(), name='mailing'),
+    path('mailing/', cache_page(60)(MailingListView.as_view()), name='mailing'),
     path('mailing/<int:pk>/', MailingDetailView.as_view(), name='view'),
     path('mailing/update/<int:pk>/', MailingUpdateView.as_view(), name='update'),
     path('mailing/delete/<int:pk>/', MailingDeleteView.as_view(), name='delete'),
